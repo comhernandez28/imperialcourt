6102daa6a52c999b01922e2cf1091f226b9b3e76
comhernandez28 <comhernandez28@gmail.com>
2017-06-05 16:52:26 -0700

    modified navbar

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
     "use strict"; // Start of use strict
 
     // Closes responsive menu when a link is clicked
-    $('.navbar-collapse>ul>li>a').click(function() {
-        $('.navbar-collapse').collapse('hide');
-    });
+    //$('.navbar-collapse>ul>li>a').click(function() {
+        //$('.navbar-collapse').collapse('hide');
+    //});
 
 
 })(jQuery); // End of use strict
